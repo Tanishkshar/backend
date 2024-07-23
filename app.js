ce310224e2e3336a06fdf9e1ea0dab53b294b473
Tanishk <TanishkSharma795@gmail.com>
2024-07-23 19:02:12 +0530

    changes

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ dbConnection();
 // };
 // const cors = require("cors");
 const corsOptions = {
-  origin: "http://127.0.0.1:5173",
+  origin: "https://mern-ecomm-kibm.vercel.app/",
   credentials: true, // Allow credentials (cookies, authentication)
 };
 
