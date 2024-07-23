faf6f9f158b3e4576fc6c7466aa7a7b0dff3058d
Tanishk <TanishkSharma795@gmail.com>
2024-07-23 19:24:49 +0530

    fixes

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ dbConnection();
 // };
 // const cors = require("cors");
 const corsOptions = {
-  origin: "https://frontend-seven-gamma-67.vercel.app/",
+  origin: ["*"],
   credentials: true, // Allow credentials (cookies, authentication)
 };
 
